d4b2a09a3f81c26953ccea936ab40902d7368b4a
Margarita Mityagovskaya <lucia.666.lucia@gmail.com>
2023-01-16 17:02:38 +0300

    week-1

--- .eslintrc.js
+++ .eslintrc.js
@@ -12,17 +12,27 @@ module.exports = {
   },
   rules: {
     'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'vue/multi-word-component-names': 'off',
+    'no-multiple-empty-lines': [2, {
+      "max": 99999,
+      "maxEOF": 0
+    }],
+    'eol-last': 'off',
+    'semi': 0,
+    'avoidEscape': 0,
+    'allowTemplateLiterals': 0,
+    'quotes': 0,
+    'comma-dangle': 0,
+    'space-before-function-paren': 0,
   },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
+  overrides: [{
+    files: [
+      '**/__tests__/*.{j,t}s?(x)',
+      '**/tests/unit/**/*.spec.{j,t}s?(x)'
+    ],
+    env: {
+      jest: true
     }
-  ]
+  }]
 }
---
+++ src/components/card/card.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="card">
+    <div class="class__title_wrap mb-16">
+      <h2 class="card__title">{{ title }}</h2>
+    </div>
+    <div class="card__desc_wrap mb-32">
+      <div class="card__desc">{{ desc }}</div>
+    </div>
+    <div class="card__stats">
+      <stats :star="star" :fork="fork" />
+    </div>
+  </div>
+</template>
+
+<script>
+import { stats } from "../../components/stats";
+export default {
+  components: {
+    stats,
+  },
+  props: {
+    title: {
+      type: String,
+      required: true,
+    },
+    desc: {
+      type: String,
+      required: true,
+    },
+    star: {
+      type: Number,
+      required: true,
+    },
+    fork: {
+      type: Number,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+.card {
+  padding: 20px 24px;
+  background: #ffffff;
+  border: 1px solid #f1f1f1;
+  box-shadow: 0px 4px 40px rgba(0, 0, 0, 0.07);
+  border-radius: 10px;
+  margin-bottom: 18px;
+}
+</style>
