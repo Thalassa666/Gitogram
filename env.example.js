399e66691183d237ac19da67b6a7929ec7fe92a6
Thalassa666 <107143120+Thalassa666@users.noreply.github.com>
2023-02-17 21:30:05 +0300

    Update env.example.js

--- env.example.js
+++ env.example.js
@@ -1,4 +1,6 @@
 export default {
-  clientId: 'b3bbe06e20f921691504',
-  clientSecret: '01a3ef5c88565322e4d38b67b69ef86919e91baa'
+  clientId: '',
+  clientSecret: ''
 }
+
+// need Id and secret from Github OAuth
